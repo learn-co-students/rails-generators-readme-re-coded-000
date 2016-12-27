725ab794348779bb28da7230a824ac9c93248e89
shereen messi <shereenmessi@hotmail.com>
2016-12-27 21:04:13 +0000

    Done.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,21 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  it "has a name" do
-    @author = Author.new
-    @author.name = "Joe Burgess"
-    expect(@author.name).to eq("Joe Burgess")
-  end
-
-  it "has a genre" do
-    @author = Author.new
-    @author.genre = "Fiction"
-    expect(@author.genre).to eq("Fiction")
-  end
-
-  it "has a bio" do
-    @author = Author.new
-    @author.bio = "I'm a Teacher!"
-    expect(@author.bio).to eq("I'm a Teacher!")
+  describe 'Author' do
+    it "has a name field" do
+      @author = Author.create(name: "Camus", genre: "test", bio: "French Writer")
+      expect(@author.name).to eq("Camus")
+    end
   end
 end
